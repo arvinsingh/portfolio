a5fcdd4059a13714b8fdec8d9a54e6ef9ce1a72b
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-02-14 21:48:02 +0000

    modified: updated project list

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,27 @@ const imageAltText = "Keybard, PC and code on a desk";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Exploratory reLiable Information Scanning in Articles (ELISA)",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A fake news and clickbait detection project that uses machine learning to classify news articles as reliable or unreliable.",
+    url: "https://github.com/arvinsingh/ELISA",
   },
   {
-    title: "Web Development for Beginners",
+    title: "ClusTMPay - Notification reduction using Machine Learning.",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A project that uses machine learning to reduce number of irrelevant notification recieved by a user from an ecommerce site.",
+    url: "https://github.com/arvinsingh/SIH2019-PayTM-SJ1-MachineWorks",
   },
   {
-    title: "My Resume Site",
+    title: "Machine Learning and Deep Learning",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A project that uses Convolutional Neural Networks to classify images from the CIFAR-10 dataset.",
+    url: "https://github.com/arvinsingh/ML-and-DL",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "Automation Anywhere Bots",
+    description: "Bots to run automated tasks.",
+    url: "https://github.com/arvinsingh/AutomationAnywhereBots",
   },
 ];
 
