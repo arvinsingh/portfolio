05f2c7d3ebda9b14407c589f9d2b16cc85eaa31c
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-02-14 21:24:20 +0000

    modified: added anim to scroll arrow

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -33,7 +33,11 @@ const Home = ({ name, title }) => {
         <h2>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        <img
+          src={arrowSvg}
+          style={{ height: "3rem", width: "3rem", animation: "upAndDown 2s infinite" }}
+          alt={imageAltText}
+        />
       </div>
     </section>
   );
