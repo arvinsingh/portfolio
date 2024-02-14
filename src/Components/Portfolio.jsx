c699d33fb4206e8582e0afdc649ef90ccbdc16b5
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-02-14 19:53:26 +0000

    modified: added new image of workstation and desc

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/keyboard-pc-code.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Keybard, PC and code on a desk";
 
 /**
  * Project list
